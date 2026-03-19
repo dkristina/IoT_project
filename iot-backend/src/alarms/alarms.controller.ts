c03f4ddfe727ccad47bd91f0888e512c0049ba66
Kristina <dimitrijevick68@gmail.com>
2026-03-19 19:39:27 +0100

    sredjen alarm controller i modul

--- iot-backend/src/alarms/alarms.controller.ts
+++ iot-backend/src/alarms/alarms.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, ParseIntPipe } from '@nestjs/common';
 import { AlarmsService } from './alarms.service';
 import { CreateAlarmDto } from './dto/create-alarm.dto';
 import { UpdateAlarmDto } from './dto/update-alarm.dto';
@@ -8,27 +8,28 @@ export class AlarmsController {
   constructor(private readonly alarmsService: AlarmsService) {}
 
   @Post()
-  create(@Body() createAlarmDto: CreateAlarmDto) {
+  async create(@Body() createAlarmDto: CreateAlarmDto) {
     return this.alarmsService.create(createAlarmDto);
   }
 
   @Get()
-  findAll() {
+  async findAll(@Query('sensorId') sensorId?: string) {
+    if (sensorId) return this.alarmsService.findBySensor(+sensorId);
     return this.alarmsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.alarmsService.findOne(+id);
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.alarmsService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAlarmDto: UpdateAlarmDto) {
-    return this.alarmsService.update(+id, updateAlarmDto);
+  async update(@Param('id', ParseIntPipe) id: number, @Body() updateAlarmDto: UpdateAlarmDto) {
+    return this.alarmsService.update(id, updateAlarmDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.alarmsService.remove(+id);
+  async remove(@Param('id', ParseIntPipe) id: number) {
+    return this.alarmsService.remove(id);
   }
 }
--- iot-backend/src/alarms/alarms.module.ts
+++ iot-backend/src/alarms/alarms.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AlarmsService } from './alarms.service';
 import { AlarmsController } from './alarms.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Alarm } from './entities/alarm.entity';
+import { Sensor } from 'src/sensors/entities/sensor.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Alarm, Sensor])],
   controllers: [AlarmsController],
   providers: [AlarmsService],
+  exports: [AlarmsService],
 })
 export class AlarmsModule {}
