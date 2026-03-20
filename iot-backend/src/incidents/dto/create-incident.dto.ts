4b98e5a1faaead896fe2828f5717570c481bcf27
Kristina <dimitrijevick68@gmail.com>
2026-03-20 19:51:41 +0100

    kreiranje DTO objekata

--- iot-backend/src/incidents/dto/create-incident.dto.ts
+++ iot-backend/src/incidents/dto/create-incident.dto.ts
@@ -1,1 +1,27 @@
-export class CreateIncidentDto {}
+import { IsEnum, IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
+import { AlarmSeverity } from "src/alarms/entities/alarm.entity";
+import { IncidentStatus } from "../entities/incident.entity";
+
+export class CreateIncidentDto {
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsEnum(AlarmSeverity)
+  @IsNotEmpty()
+  severity: AlarmSeverity;
+
+  @IsEnum(IncidentStatus)
+  @IsOptional()
+  status?: IncidentStatus;
+
+  @IsNumber()
+  @IsNotEmpty()
+  sensorId: number;
+
+  //opciono, ako odmah znamo ko resava
+  @IsNumber()
+  @IsOptional()
+  assignedToId?: number;
+}
--- iot-backend/src/incidents/dto/update-incident.dto.ts
+++ iot-backend/src/incidents/dto/update-incident.dto.ts
@@ -1,4 +1,15 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateIncidentDto } from './create-incident.dto';
+import { IsDateString, IsEnum, IsOptional } from 'class-validator';
+import { IncidentStatus } from '../entities/incident.entity';
 
-export class UpdateIncidentDto extends PartialType(CreateIncidentDto) {}
+export class UpdateIncidentDto extends PartialType(CreateIncidentDto) {
+  
+  @IsEnum(IncidentStatus)
+  @IsOptional()
+  status?: IncidentStatus;
+
+  @IsDateString()
+  @IsOptional()
+  resolvedAt?: Date;
+}
