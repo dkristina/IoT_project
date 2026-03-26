f6ceb115e00deef64b4d2923379a66b4201510be
Kristina <dimitrijevick68@gmail.com>
2026-03-26 16:31:59 +0100

    izmenjen entitet, dodat indeks i optimizovan timestamp

--- iot-backend/src/measurements/entities/measurement.entity.ts
+++ iot-backend/src/measurements/entities/measurement.entity.ts
@@ -1,6 +1,7 @@
 import { Sensor } from "src/sensors/entities/sensor.entity";
-import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, Index, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 
+@Index(['sensorId', 'timestamp']) //optimizacija za brzu pretragu istorije
 @Entity()
 export class Measurement {
   @PrimaryGeneratedColumn()
@@ -9,9 +10,16 @@ export class Measurement {
   @Column('float')
   value: number;
 
-  @CreateDateColumn({ type: 'timestamptz' })
+  @Column({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
   timestamp: Date;
 
+  //eksplicitna kolona za ID senzora
+  //omogucava nam brze upite nad tabelom senzora
+  //kod spitivanja 3 merenja (smanjuje memorijski overhead)
+  @Column()
+  sensorId: number;
+
   @ManyToOne(() => Sensor, (s) => s.measurements, { onDelete: 'CASCADE'})
+  @JoinColumn({ name: 'sensorId' }) 
   sensor: Sensor; 
 }
