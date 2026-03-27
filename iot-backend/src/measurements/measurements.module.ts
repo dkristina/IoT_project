f15e94b2610aaa0d8a92aa03399a986f546817f1
Kristina <dimitrijevick68@gmail.com>
2026-03-27 02:19:32 +0100

    povezani Alarms i Incidents moduli

--- iot-backend/src/measurements/measurements.module.ts
+++ iot-backend/src/measurements/measurements.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MeasurementsService } from './measurements.service';
 import { MeasurementsController } from './measurements.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Measurement } from './entities/measurement.entity';
+import { AlarmsModule } from 'src/alarms/alarms.module';
+import { IncidentsModule } from 'src/incidents/incidents.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Measurement]),
+    AlarmsModule, 
+    IncidentsModule,
+  ],
   controllers: [MeasurementsController],
   providers: [MeasurementsService],
 })
