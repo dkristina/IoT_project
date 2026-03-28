ed9d096e41fe0aa8d0e065222f2cb7503d15b43e
Kristina <dimitrijevick68@gmail.com>
2026-03-28 16:20:42 +0100

    dodat global validation pipe

--- iot-backend/src/main.ts
+++ iot-backend/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,         // Brise polja koja nisu definisana u DTO-u
+    forbidNonWhitelisted: true, // Bacaj gresku ako neko posalje "visak" podataka
+    transform: true,         // Automatski pretvara tipove (npr. string u number)
+  }));
+
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
