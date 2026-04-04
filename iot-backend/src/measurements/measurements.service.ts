f304b741ecb600662e76841bf87cb767e1de8f27
Kristina <dimitrijevick68@gmail.com>
2026-04-04 02:00:14 +0200

    finalno uskladjivanje koda

--- iot-backend/src/measurements/measurements.service.ts
+++ iot-backend/src/measurements/measurements.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException, UseGuards } from '@nestjs/common';
 import { CreateMeasurementDto } from './dto/create-measurement.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Measurement } from './entities/measurement.entity';
