55a2a851480f33e16bd7a98811d38982f8058d2e
Kristina <dimitrijevick68@gmail.com>
2026-04-04 02:03:48 +0200

    koriscenje ROLES_KEY konstante

--- iot-backend/src/auth/guards/roles.guard.ts
+++ iot-backend/src/auth/guards/roles.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable, CanActivate, ExecutionContext, ForbiddenException } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { UserRole } from '../../users/entities/user.entity';
+import { ROLES_KEY } from '../decorators/roles.decorator';
 
 interface JwtUser {
   userId: number;
@@ -13,7 +14,7 @@ export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>('roles', [
+    const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>(ROLES_KEY, [
       context.getHandler(),
       context.getClass(),
     ]);
